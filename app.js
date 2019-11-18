66cec530aea46fe09f93a81180119e3350eab3ae
Brockston King <brockstonking@Brockstons-MacBook-Pro-2.local>
2019-11-17 21:38:41 -0700

    working

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.get('/api/test', (req, res, next) => {
   
 
 app.post('/api/send_email', (req, res, next) => {
-    debugger
   const { name, email, phone, serviceRequested, prefferedTime, notesOrQuestions } = req.body;
   var transporter = nodemailer.createTransport({
     service: 'gmail',
